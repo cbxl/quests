966ab224fc611d6c756c7d2c51b9f63ebb629056
cbxl <colinebui@gmail.com>
2020-10-19 11:54:38 +0200

    quete dico

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> dictionary = new Dictionary<char, string>();
+
+            dictionary.Add('a', "Armagnac");
+            dictionary['b'] = "Bière";
+            dictionary['c'] = "Champagne";
+
+            foreach (KeyValuePair<char, string> mot in dictionary)
+            {
+                Console.WriteLine("{0}: {1}", mot.Key, mot.Value);
+            }
+
+            dictionary.Remove('c');
+
+            foreach (KeyValuePair<char, string> mot in dictionary)
+            {
+                Console.WriteLine("{0}: {1}", mot.Key, mot.Value);
+            }
+        }
+    }
+}
