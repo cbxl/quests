069db1ae7a749a36897d95aaad070d991bb5b8fa
cbxl <colinebui@gmail.com>
2020-11-23 11:42:26 +0100

    quête routage

---
+++ Routage/Routage/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Routage.Controllers
+{
+    
+    public class HelloController
+    {
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+}
---
+++ Routage/Routage/Controllers/HelloControllerAttribut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Routage.Controllers
+{
+
+    public class HelloControllerAttribut : Controller
+    {
+        [Route("hello")]
+        [Route("hello/dit")]
+        public String DitHello()
+        {
+            return "Hello !";
+        }
+
+        [Route("hello/crie")]
+        public String CrieHello()
+        {
+            return "HELLO !";
+        }
+    }
+
+}
