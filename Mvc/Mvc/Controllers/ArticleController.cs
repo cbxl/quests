3dcb88a65f73ab205746fd39c32b575ad2dcfa73
cbxl <colinebui@gmail.com>
2020-11-25 16:42:40 +0100

    quête MVC

---
+++ Mvc/Mvc/Controllers/ArticleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Mvc/Mvc/Models/Articles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }       
+        public string Content { get; set; }
+    }
+}
