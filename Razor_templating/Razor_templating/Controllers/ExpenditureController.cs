b5a4607cefdb4dcbba589b276003cab1d3782762
cbxl <colinebui@gmail.com>
2020-12-07 15:15:09 +0100

    quete Razor templating

---
+++ Razor_templating/Razor_templating/Controllers/ExpenditureController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Razor_templating.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_templating.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<ExpenditureModel> expenditures = new List<ExpenditureModel>
+            {new ExpenditureModel{Name ="PC", Date = "07/12/20", Price = 150.20 },
+            new ExpenditureModel{Name ="table", Date = "06/12/20", Price = 100.20 },
+            new ExpenditureModel{Name ="chaise", Date = "04/12/20", Price = 10.80 }
+            };
+
+            return View(expenditures);
+        }
+
+        
+    }
+}
---
+++ Razor_templating/Razor_templating/Models/ExpenditureModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Razor_templating.Models
+{
+    public class ExpenditureModel
+    {
+        public Int32 Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Date{ get; set; }
+
+        public Double Price { get; set; }
+
+    }
+}
