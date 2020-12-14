8455fe84f9b08d7536fdd9f2a0b91c0cfed1d50f
cbxl <colinebui@gmail.com>
2020-12-14 23:00:49 +0100

    quête Webclient API REST

---
+++ WebClient_API_REST/WebClient_API_REST/Controllers/PlanetsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebClient_API_REST.Models;
+
+namespace WebClient_API_REST.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+
+            Console.WriteLine("Indiquez le numéro de la planète souhaitée");
+            Int32 planetNumber = Convert.ToInt32(Console.ReadLine());
+
+            WebClient client = new WebClient();
+
+            String planetSelectedResponse = client.DownloadString("https://swapi.dev/api/planets/"+planetNumber+"/");    
+            PlanetsModel planetSelected = JsonConvert.DeserializeObject<PlanetsModel>(planetSelectedResponse);           
+
+            return View(planetSelected);
+        }
+    }
+}
---
+++ WebClient_API_REST/WebClient_API_REST/Models/Planets.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClient_API_REST.Models
+{
+    public class PlanetsModel
+    {
+        public string Name { get; set; }
+        public string Rotation_Period { get; set; }
+
+        public string Orbital_Period { get; set; }
+        public string Diameter { get; set; }
+        public string Climate { get; set; }
+
+        public string Gravity { get; set; }
+        public string Terrain { get; set; }
+
+        public string Surface_Water { get; set; }
+
+        public string Population { get; set; }
+
+        public string[] Residents { get; set; }
+
+        public string[] Films { get; set; }
+
+        public string Created { get; set; }
+
+        public string Edited { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
