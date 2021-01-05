7e82cfac8a8f87dafc9ce89505c962de0c759fdc
cbxl <colinebui@gmail.com>
2021-01-05 14:19:16 +0100

    modification HomeController

--- RESTful_Webservices/RESTful_Webservices/Controllers/HomeController.cs
+++ RESTful_Webservices/RESTful_Webservices/Controllers/HomeController.cs
@@ -13,11 +13,30 @@ namespace RESTful_Webservices.Controllers
 {
     public class HomeController : Controller
     {
+        public WebClient _client = new WebClient();
+        public String _uri = "https://jsonplaceholder.typicode.com/";
         
-        public IActionResult Index()
-        {
+        
 
-            return View();
+        public IActionResult Index(Int32 id)
+        {
+            if (id <= 0)
+            {
+                return View();
+            }
+            else
+            {
+                string responseUsers = _client.DownloadString(_uri +"users/" + id );
+                User user = JsonConvert.DeserializeObject<User>(responseUsers);
+
+                string responsePosts = _client.DownloadString(_uri + "posts/?userId=" + id);
+                IEnumerable<Post> post = JsonConvert.DeserializeObject<IEnumerable<Post>> (responsePosts);
+
+                return View(post);
+            }
         }
+
+       
+
     }
 }
